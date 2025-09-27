c5fffbe6999bef2e6f36a41806030fba92ce49c9
jonsol <django.palmer@gmail.com>
2025-09-27 17:54:13 +0200

    test

--- time_series_visualizer_test.py
+++ time_series_visualizer_test.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import datetime
 import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
-import matplotlib.dates as dates
+import matplotlib.dates as mdates
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
 df = pd.read_csv('fcc-forum-pageviews.csv')
@@ -14,11 +15,15 @@ df = df.loc[(df['value'] >= df['value'].quantile(0.025))
 & (df['value'] <= df['value'].quantile(0.975))]
 
 
-    plt.figure(figsize=(8, 6))
+df_line_chart = df.copy()
 
-    plt.plot(df['date'], df['value'])
-    plt.locator_params(axis='x', nbins=4)
-    plt.xlabel("Date")
-    plt.ylabel("Page Views")
-    plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
-    plt.show()
+x = df_line_chart['date']
+y = df_line_chart['value']
+
+pd.TimeSeries(y, index=x).plot()
+
+plt.xlabel("Date")
+plt.ylabel("Page Views")
+plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+plt.show()
+
